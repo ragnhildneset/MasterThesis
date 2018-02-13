7897d2d72acd44ece8c69c2618385b35b370d4d6
Ragnhild Neset <ragcecnes@gmail.com>
2018-02-13 13:43:01 +0100

    fix directory name error

--- scripts/add_directory_to_imagename.py
+++ scripts/add_directory_to_imagename.py
@@ -10,7 +10,7 @@ parser.add_argument('new_labels_file')
 args = parser.parse_args()
 
 data = pd.read_csv(os.path.join(args.dir_path, args.labels_file))
-data['image'] = '/new_image_dir/' + data['image'].astype(str)
+data['image'] = args.new_image_dir + "/" + data['image'].astype(str)
 
 data.to_csv(args.new_labels_file, index=False)
 print(data)
