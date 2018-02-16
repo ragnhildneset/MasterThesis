3973674484b6b0436d0ca485e2a95a2c79435354
Ragnhild Neset <ragcecnes@gmail.com>
2018-02-16 11:24:36 +0100

    simplify dataset restructure  script

--- scripts/add_directory_to_imagename.py
+++ scripts/add_directory_to_imagename.py
@@ -4,14 +4,12 @@ import os
 
 parser = argparse.ArgumentParser()
 parser.add_argument('dir_path')
-parser.add_argument('labels_file')
 parser.add_argument('new_image_dir')
-parser.add_argument('new_labels_file')
 args = parser.parse_args()
 
-data = pd.read_csv(os.path.join(args.dir_path, args.labels_file))
+data = pd.read_csv(os.path.join(args.dir_path, "labels.csv"))
 data['image'] = args.new_image_dir + "/" + data['image'].astype(str)
 
-data.to_csv(args.new_labels_file, index=False)
+data.to_csv(args.new_image_dir + ".csv", index=False)
 print(data)
 
