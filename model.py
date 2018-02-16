5d497f6268d114727bea0157014ad2e5454fec3d
Ragnhild Neset <ragcecnes@gmail.com>
2018-02-16 13:40:36 +0100

    Make model.py save to  output directory

--- model.py
+++ model.py
@@ -4,17 +4,20 @@ import architecture
 import utilities
 import argparse
 import data_processing
+import os
 
 from keras.callbacks import TensorBoard
+
+MODEL_DIR = "output/models"
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Train a neural network to autonomously drive a virtual car. Example syntax:\n\npython model.py -d udacity_dataset -m model.h5')
     parser.add_argument('--dataset-directory', '-d', dest='dataset_directory', type=str, required=True, help='Required string: Directory containing driving log and images.')
-    parser.add_argument('--model-path', '-m', dest='model_path', type=str, required=True, help='Required string: Name of model e.g model.h5.')
+    parser.add_argument('--model-name', '-m', dest='model_name', type=str, required=True, help='Required string: Name of model e.g model.h5.')
     parser.add_argument('--cpu-batch-size', '-c', dest='cpu_batch_size', type=int, required=False, default=1000000, help='Optional integer: Image batch size that fits in system RAM. Default 1000.')
     parser.add_argument('--gpu-batch-size', '-g', dest='gpu_batch_size', type=int, required=False, default=512, help='Optional integer: Image batch size that fits in VRAM. Default 512.')
     parser.add_argument('--randomize', '-r', dest='randomize', type=bool, required=False, default=False, help='Optional boolean: Randomize and overwrite driving log. Default False.')
-    parser.add_argument('--tensorboard-dir', '-t', dest='tensorboard_dir', type=str, required=False, default='logs', help='The directory in which the tensorboard logs should be saved.')
+    parser.add_argument('--tensorboard-dir', '-t', dest='tensorboard_dir', type=str, required=False, default='output/logs', help='The directory in which the tensorboard logs should be saved.')
     args = parser.parse_args()
 
     if args.randomize:
@@ -58,4 +61,7 @@ if __name__ == "__main__":
 
         model.fit(X_batch, Y_batch, validation_data=(X_valid, Y_valid), shuffle=True, epochs=15, batch_size=args.gpu_batch_size, callbacks=[tensorboard])
         measurement_index += args.cpu_batch_size
-    model.save(args.model_path)
+
+    if not os.path.exists(MODEL_DIR):
+        os.makedirs(MODEL_DIR)
+    model.save(os.path.join(MODEL_DIR, args.model_name))
