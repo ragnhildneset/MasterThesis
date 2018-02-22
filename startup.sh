29eb67403b40e3c3741599b5db17c82b8052b4f7
Anna Kastet <annakastet@gmail.com>
2018-02-22 11:13:54 +0100

    Add ports for jupyter notebooks

--- startup.sh
+++ startup.sh
@@ -1,5 +1,5 @@
 {
-  tmux new -d -s tensorflow 'nvidia-docker run -it -v "$(pwd)":/ai annaragnhild-tensorflow'
+  tmux new -d -s tensorflow 'nvidia-docker run -it -v "$(pwd)":/ai -p 8888:8888 annaragnhild-tensorflow'
 } && {
   tmux new -d -s tensorboard 'nvidia-docker run -it -v "$(pwd)"/output/logs:/ai/logs -p 6006:6006 gcr.io/tensorflow/tensorflow:latest-gpu tensorboard --logdir=/ai/logs --port=6006'
 } && {
