3014bc7d74db342342c0731b535902026f960ba8
Ragnhild Neset <ragcecnes@gmail.com>
2018-02-28 14:15:43 +0100

    Move heat map to visualization

--- visualisation.py
+++ visualisation.py
@@ -1,4 +1,11 @@
 import cv2
+import utilities
+import matplotlib.pyplot as plt
+import os
+import data_processing
+
+from vis.visualization import visualize_cam, overlay
+
 
 SEABORN_RED = (82, 78, 196)
 SEABORN_GREEN = (104, 168, 85)
@@ -31,3 +38,18 @@ def process_img_for_angle_visualization(img, angle, pred_angle, frame):
         cv2.line(img, (int(w/2), int(h)), (int(w/2-pred_angle*w/4), int(h/2)),
                  SEABORN_RED, thickness=3)
     return img
+
+
+def make_and_save_heat_maps(model, sample, layer, figure_folder):
+    utilities.make_folder(figure_folder)
+
+    plt.figure()
+    for i in range(sample["images"].shape[0]):
+        image = sample["images"][i]
+        grads = visualize_cam(model, layer, filter_indices=20, seed_input=image)
+
+        display_image = data_processing.un_normalize_color(image)
+        plt.imshow(overlay(grads, cv2.convertScaleAbs(display_image)))
+
+        figure_name = sample["image_names"][i].replace("/", "_")
+        plt.savefig(os.path.join(figure_folder, figure_name))
