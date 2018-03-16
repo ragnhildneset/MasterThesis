162bdb13119bb8cec0824d3aebad79e6d420f006
Anna Kastet <annakastet@gmail.com>
2018-03-16 11:14:22 +0100

    Add two simplified Bojarski models

--- architecture.py
+++ architecture.py
@@ -28,7 +28,6 @@ class Model:
 
 class Bojarski_Model(Model):
     def get_model(self):
-
         model = Sequential()
         model.add(Conv2D(24, (5, 5), strides=(2, 2), activation="relu", input_shape=(67, 320, 3)))
         model.add(Conv2D(36, (5, 5), strides=(2, 2), activation="relu"))
@@ -38,6 +37,34 @@ class Bojarski_Model(Model):
         model.add(Flatten())
         model.add(Dense(100, activation="relu"))
         model.add(Dense(50, activation="relu"))
+        model.add(Dense(10, activation="relu"))
+        model.add(Dense(self.NOF_OUTPUTS))
+        model.compile(optimizer="adam", loss="mse", metrics=['accuracy'])
+        return model
+
+class Simplified_Bojarski_Model(Model):
+    def get_model(self):
+        model = Sequential()
+        model.add(Conv2D(24, (5, 5), strides=(2, 2), activation="relu", input_shape=(67, 320, 3)))
+        model.add(Conv2D(36, (5, 5), strides=(2, 2), activation="relu"))
+        model.add(Conv2D(48, (5, 5), strides=(2, 2), activation="relu"))
+        model.add(Conv2D(64, (3, 3), strides=(1, 1), activation="relu"))
+        model.add(Flatten())
+        model.add(Dense(70, activation="relu"))
+        model.add(Dense(10, activation="relu"))
+        model.add(Dense(self.NOF_OUTPUTS))
+        model.compile(optimizer="adam", loss="mse", metrics=['accuracy'])
+        return model
+
+
+class Very_Simplified_Bojarski_Model(Model):
+    def get_model(self):
+        model = Sequential()
+        model.add(Conv2D(24, (5, 5), strides=(2, 2), activation="relu", input_shape=(67, 320, 3)))
+        model.add(Conv2D(36, (5, 5), strides=(2, 2), activation="relu"))
+        model.add(Conv2D(64, (3, 3), strides=(1, 1), activation="relu"))
+        model.add(Flatten())
+        model.add(Dense(35, activation="relu"))
         model.add(Dense(10, activation="relu"))
         model.add(Dense(self.NOF_OUTPUTS))
         model.compile(optimizer="adam", loss="mse", metrics=['accuracy'])
