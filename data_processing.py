f7bf3f0a382280b3fef559c4de7010a94f4289e8
Ragnhild Neset <ragcecnes@gmail.com>
2018-04-04 12:52:57 +0200

    add random erasing

--- data_processing.py
+++ data_processing.py
@@ -10,6 +10,7 @@ def batch_generator(samples, dataset_path, batch_size, img_size=(67, 320),
     images = np.zeros((batch_size, img_size[0], img_size[1], 3))
     steers = np.zeros((batch_size, nof_outputs))
 
+    random_erasing = RandomErasing()
     while True:
         i = 0
         for index in np.random.permutation(samples.shape[0]):
@@ -19,13 +20,12 @@ def batch_generator(samples, dataset_path, batch_size, img_size=(67, 320),
             image_path = samples.iloc[index, 2]
             image = utilities.load_image(dataset_path, image_path)
 
-            #
             # Augmentation
-            #
             if np.random.rand() < 0.6:
                 image, angle = flip(image, angle)
-
             image = random_brightness(image)
+            if np.random.rand() < 0.6:
+                image = random_erasing(image)
 
             images[i] = preprocess(image, img_size)
             steers[i] = [angle, speed] if (include_angles and include_speed) \
@@ -96,3 +96,36 @@ def random_brightness(image):
     image1 = cv2.cvtColor(image1, cv2.COLOR_HSV2RGB)
     return image1
 
+
+
+import numpy as np
+import random
+import math
+
+class RandomErasing(object):
+    def __init__(self, sl=0.02, sh=0.4, r1=0.3, mean=[127, 127, 127]):
+        self.mean = mean
+        self.sl = sl
+        self.sh = sh
+        self.r1 = r1
+
+    def __call__(self, img):
+
+        for attempt in range(100):
+            area = img.shape[0] * img.shape[1]
+
+            target_area = random.uniform(self.sl, self.sh) * area
+            aspect_ratio = random.uniform(self.r1, 1 / self.r1)
+
+            h = int(round(math.sqrt(target_area * aspect_ratio)))
+            w = int(round(math.sqrt(target_area / aspect_ratio)))
+            if w < img.shape[1] and h < img.shape[0]:
+                x1 = random.randint(0, img.shape[0] - h)
+                y1 = random.randint(0, img.shape[1] - w)
+
+                img[x1:x1 + h, y1:y1 + w, 0] = self.mean[0]
+                img[x1:x1 + h, y1:y1 + w, 1] = self.mean[1]
+                img[x1:x1 + h, y1:y1 + w, 2] = self.mean[2]
+                return img
+
+        return img
